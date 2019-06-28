9b612ed2c5adca145b8ce25090f6d28fa3a860b2
DSilwal <deepen.silwal@ttu.edu>
2019-06-28 12:22:01 -0500

    commit with all the necessary modifications

--- tests/sum.ts
+++
@@ -1,5 +0,0 @@
-//https://basarat.gitbooks.io/typescript/docs/testing/jest.html
-//simple example function to test jest
-export const sum
-  = (...a: number[]) =>
-    a.reduce((acc, val) => acc + val, 0);
